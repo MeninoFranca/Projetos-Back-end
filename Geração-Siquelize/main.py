a6fce1edcdf93ad6939d2cabccfa376c73647dd7
meninofranca <luisfilipelima08@gmail.com>
2024-09-15 11:53:29 -0300

    Extraindo info e armazenando na lista columns[]

--- Geração-Siquelize/main.py
+++ Geração-Siquelize/main.py
@@ -5,3 +5,13 @@ import os
 def create_siquelize(tables, path):
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('model_template.js')
+
+    for table_name, table in tables.items():
+        columns = []
+        for column in table.columns:
+            columns.append({
+                'name': column.name,
+                'data_type': str(column.type),
+                'nullable': column.nullable,
+                'primary_key': column.primary_key
+            })
